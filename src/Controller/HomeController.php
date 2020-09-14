76dd88a2436364891c0d767bf790da761031c5c6
Mamadou-Sow <petitwizi2@gmail.com>
2020-09-14 22:06:19 +0000

    Chargement des données de la page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/test", name="home2")
+     */
+    public function test()
+    {
+        return $this->render('home/test.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
